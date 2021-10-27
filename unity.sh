09c39c4d3e7d2e440402c6ea8f66325a43276812
Simon Schmid <mail@sschmid.com>
2021-10-27 17:32:51 +0200

    Migrate to using secrets

--- unity.sh
+++ unity.sh
@@ -4,19 +4,23 @@ unity::_new() {
   echo '# unity'
   echo 'UNITY_PROJECT_PATH=.
 UNITY=/Applications/Unity/Hub/Editor/2019.4.19f1/Unity.app/Contents/MacOS/Unity
-# Potentially sensitive data. Do not commit.
-UNITY_SERIAL="AB-1234-5678-1234-5678"
-UNITY_USER="user"
-UNITY_PASSWORD="password"'
+# secrets:
+unity.serial
+unity.user
+unity.password'
 }
 
 unity::batchmode() {
+  local serial user password
+  serial="$(bee::secrets unity.serial)"
+  user="$(bee::secrets unity.user)"
+  password="$(bee::secrets unity.password)"
   "${UNITY}" \
     -projectPath "${UNITY_PROJECT_PATH}" \
     -batchmode \
     -nographics \
     -logfile \
-    -serial "${UNITY_SERIAL}" -username "$UNITY_USER" -password "${UNITY_PASSWORD}" \
+    -serial "${serial}" -username "${user}" -password "${password}" \
     -quit "$@"
 }
 
