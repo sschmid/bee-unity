2b35780d2a414998253e3b7149fce7c196d8d731
Simon Schmid <mail@sschmid.com>
2021-12-14 22:32:16 +0100

    Update plugin according to bee 1.0.0 specs

--- unity.bash
+++ unity.bash
@@ -1,26 +1,45 @@
-#!/usr/bin/env bash
+: "${UNITY_PROJECT_PATH:=.}"
 
-unity::_new() {
-  echo '# unity'
-  echo 'UNITY_PROJECT_PATH=.
-UNITY=/Applications/Unity/Hub/Editor/2019.4.19f1/Unity.app/Contents/MacOS/Unity
-# secrets:
-unity.serial
-unity.user
-unity.password'
+unity::help() {
+  cat << 'EOF'
+Useful commands for Unity development - https://github.com/sschmid/bee-unity
+
+template:
+
+  UNITY_PROJECT_PATH=. # default
+  UNITY="/Applications/Unity/Hub/Editor/2021.2.7f1/Unity.app/Contents/MacOS/Unity"
+
+secrets:
+
+  UNITY_SERIAL
+  UNITY_USER
+  UNITY_PASSWORD
+
+usage:
+
+  batchmode [<batchmode-arguments>]        open Unity in batchmode and quit
+  execute_method [<batchmode-arguments>]   open Unity in batchmode, execute the specified method and quit
+                                           e.g. bee unity execute_method Build
+  sync_solution [<batchmode-arguments>]    open Unity in batchmode, sync C# project and quit
+
+bee dependencies:
+
+  none
+
+dependencies:
+
+  Unity   https://unity3d.com
+
+EOF
 }
 
 unity::batchmode() {
-  local serial user password
-  serial="$(bee::secrets unity.serial)"
-  user="$(bee::secrets unity.user)"
-  password="$(bee::secrets unity.password)"
   "${UNITY}" \
     -projectPath "${UNITY_PROJECT_PATH}" \
     -batchmode \
     -nographics \
     -logfile \
-    -serial "${serial}" -username "${user}" -password "${password}" \
+    -serial "${UNITY_SERIAL}" -username "${UNITY_USER}" -password "${UNITY_PASSWORD}" \
     -quit "$@"
 }
 
